ab716984232a79b07eb945d2f0154e766aacec10
maheshacharya <m.acharya@onehippo.com>
2016-05-05 19:17:34 -0400

    initial commit

---
+++ site/src/main/java/org/meetup/bostonjava/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.meetup.bostonjava.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="bostonjavameetup:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
