cfa0acc7b89899cd530c3a55067912ff793b66ea
maheshacharya <m.acharya@onehippo.com>
2016-05-08 06:31:54 -0400

    Calendar components moved to components package.

--- site/src/main/java/org/meetup/bostonjava/components/CalendarComponentInfo.java
+++ site/src/main/java/org/meetup/bostonjava/components/CalendarComponentInfo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.meetup.bostonjava.rest;
+package org.meetup.bostonjava.components;
 
 import org.hippoecm.hst.core.parameters.JcrPath;
 import org.hippoecm.hst.core.parameters.Parameter;
@@ -12,39 +12,40 @@ import org.hippoecm.hst.core.parameters.Parameter;
  * @author achxis-macbook
  */
 
-public interface CalendarInfo {
+public interface CalendarComponentInfo {
     static final String DEFAULT_PICKER_PATH = "/content/documents";
 
     @Parameter(name = "title", defaultValue = "Calendar", displayName = "Title")
-    public String getTitle();
+    String getTitle();
 
     @Parameter(name = "width", defaultValue = "320", displayName = "Width")
-    public int getWidth();
+    int getWidth();
 
     @Parameter(name = "height", defaultValue = "340", displayName = "Height")
-    public int getHeight();
+    int getHeight();
 
     @Parameter(name = "maxYears", defaultValue = "20", displayName = "Maximum Number of Years")
-    public int getMaxYears();
+    int getMaxYears();
 
     @Parameter(name = "futureYears", defaultValue = "4", displayName = "Future Years")
-    public int getFutureYears();
+    int getFutureYears();
 
     @Parameter(name = "backgroundColor", defaultValue = "rgba(255,255,255,0.96)", displayName = "Background Color")
-    public String getBackgroundColor();
+    String getBackgroundColor();
+
     @Parameter(name = "color", defaultValue = "#000", displayName = "Color")
-    public String getColor();
+    String getColor();
 
     @Parameter(name = "calendarClassName", defaultValue = "event-calendar", displayName = "Calendar Class Name")
-    public String getCalendarClassName();
+    String getCalendarClassName();
 
 
     @Parameter(name = "padding", defaultValue = "0", displayName = "Padding")
-    public String getPadding();
+    String getPadding();
 
 
     @Parameter(name = "dataRestEndpoint", defaultValue = "0", displayName = "Data Rest Endpoint")
-    public String getRestEndpoint();
+    String getRestEndpoint();
 
     @Parameter(name = "infoDocument", required = false, displayName = "Info Document (Pick a Simple Document)")
     @JcrPath(isRelative = true, pickerInitialPath = DEFAULT_PICKER_PATH)
